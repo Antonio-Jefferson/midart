f0add24bd8ebca366d162539e67905e8d610b567
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-05-19 01:00:37 -0400

    feat: add side menu

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ListPostComponent } from '../../list-post/list-post.component';
 import { TopMenuComponent } from '../../top-menu/top-menu.component';
 import { MenuMobileComponent } from '../../menu-mobile/menu-mobile.component';
+import { SideMenuComponent } from '../../side-menu/side-menu.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ListPostComponent, TopMenuComponent, MenuMobileComponent],
+  imports: [ListPostComponent, TopMenuComponent, MenuMobileComponent, SideMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
