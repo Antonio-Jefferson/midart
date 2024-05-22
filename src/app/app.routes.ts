be1ce2097d1b185b234fa8c5c54c688da71bf6b9
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-05-21 23:12:04 -0400

    feat(routes): add routes for groups, events, notifications, and favorites

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,21 +6,29 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { DashboardLayoutComponent } from './components/layout/dashboard-layout/dashboard-layout.component';
 import { NotFoundPageComponent } from './components/pages/not-found-page/not-found-page.component';
 import { AuthGuardService } from './guards/auth-guard.service';
+import { GroupsComponent } from './components/pages/groups/groups.component';
+import { EventsComponent } from './components/pages/events/events.component';
+import { FavoritesComponent } from './components/pages/favorites/favorites.component';
+import { NotificationsComponent } from './components/pages/notifications/notifications.component';
 
 export const routes: Routes = [
     {path: "",component: LoginComponent},
     {path: "register",component: RegisterComponent},
-    
-    { 
+
+    {
         path: "dashboard",
         component: DashboardLayoutComponent,
         canActivate: [AuthGuardService],
         children: [
             { path: "home", component: HomeComponent },
             { path: "profile", component: ProfileComponent },
+            { path: "groups", component: GroupsComponent },
+            { path: "events", component: EventsComponent },
+            { path: "notifications", component: NotificationsComponent },
+            { path: "favorites", component: FavoritesComponent },
         ]
     },
     {path: 'dashboard', redirectTo: 'dashboard/home', pathMatch: 'full' },
     {path: "**", component: NotFoundPageComponent},
-    
+
 ];
--- src/app/components/side-navegation/side-navegation.component.ts
+++ src/app/components/side-navegation/side-navegation.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-side-navegation',
   standalone: true,
-  imports: [],
+  imports: [ MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './side-navegation.component.html',
   styleUrl: './side-navegation.component.scss'
 })
