6237e2d2424eba8d131f728c9cef4e9c5bc88c18
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-05-21 23:42:26 -0400

    style(responsiveness): apply responsive design with room for improvement

--- src/app/components/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/components/layout/dashboard-layout/dashboard-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarDashboardComponent } from '../../sidebar-dashboard/side-bar-dashboard.component';
 import { SideNavegationComponent } from '../../side-navegation/side-navegation.component';
+import { SideMenuComponent } from '../../side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterOutlet, SideBarDashboardComponent, SideNavegationComponent],
+  imports: [RouterOutlet, SideBarDashboardComponent, SideNavegationComponent, SideMenuComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss'
 })
--- src/app/components/list-post/list-post.component.ts
+++ src/app/components/list-post/list-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { CardPostComponent } from '../card-post/card-post.component';
+import { CardPostComponent } from './card-post/card-post.component';
+import { TopMenuComponent } from '../top-menu/top-menu.component';
 
 @Component({
   selector: 'app-list-post',
   standalone: true,
-  imports: [CardPostComponent],
+  imports: [CardPostComponent, TopMenuComponent],
   templateUrl: './list-post.component.html',
   styleUrl: './list-post.component.scss'
 })
