8fe9e295171bfede907f7e0655dc21c50b817b38
Antonio Jefferson <antjeffersonbatista@gmail.com>
2024-05-21 23:38:19 -0400

    feat: add route in menu mobile

--- src/app/components/menu-mobile/menu-mobile.component.ts
+++ src/app/components/menu-mobile/menu-mobile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu-mobile',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './menu-mobile.component.html',
   styleUrl: './menu-mobile.component.scss'
 })
